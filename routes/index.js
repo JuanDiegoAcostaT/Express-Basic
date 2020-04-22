9625b00f6259376a155d3b5d4bac5f2c17c7752a
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-04-22 16:58:59 -0500

    Add Complete Routes Folder and view engines

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const items = [
+    {id: 1, name: 'product1'},
+    {id: 2, name: 'product2'},
+    {id: 3, name: 'product3'},
+    {id: 4, name: 'product4'}
+]
+
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title : 'My Web Store'
+    });
+});
+
+router.get('/products', (req, res, next) => {
+    res.render('products', {
+        title : 'List of Products',
+        items : items
+    });
 });
 
 module.exports = router;
